f7fb1d45bde1e9a4d96db657a7ecd32eb1b05381
7yuhuhj <jjujuj>
2026-09-18 20:54:28 +0500

    Task0

---
+++ Tyuiu.AsaulukDF.Sprint1.Task0.V0.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.AsaulukDF.Sprint1.Task0.V0.Lib
+{
+	public class DataService : ISprint1Task0V0
+	{
+		public double Calculate()
+		{
+			return 5*2 + 4*3;
+		}
+	}
+}
+			
+	
+	
+		
